49d632bc5602a3bb20ea9e0906ce1c10059bac27
xSylla <syllamodder@gmail.com>
2021-07-10 12:04:56 +0200

    Monet: Update product_model info
    Signed-off-by: xSylla <syllamodder@gmail.com>

--- lineage_monet.mk
+++ lineage_monet.mk
@@ -18,7 +18,7 @@ PRODUCT_NAME := lineage_monet
 PRODUCT_DEVICE := monet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Xiaomi Mi 10 Lite 5G
+PRODUCT_MODEL := Mi 10 Lite 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
