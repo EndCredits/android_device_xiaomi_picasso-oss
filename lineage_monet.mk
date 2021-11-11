e3b877ed6fa4f5ebbb92b2db78026de50997dacb
xSylla <syllamodder@gmail.com>
2021-11-11 09:59:37 +0100

    monet: Update blobs from V12.5.5.0.RJIEUXM

--- lineage_monet.mk
+++ lineage_monet.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := Mi 10 Lite 5G
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="monet-user 11 RKQ1.200826.002 V12.5.1.0.RJIMIXM release-keys"
+    PRIVATE_BUILD_DESC="monet-user 11 RKQ1.200826.002 V12.5.5.0.RJIEUXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/monet_global/monet:11/RKQ1.200826.002/V12.5.1.0.RJIMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/monet_eea/monet:11/RKQ1.200826.002/V12.5.5.0.RJIEUXM:user/release-keys
