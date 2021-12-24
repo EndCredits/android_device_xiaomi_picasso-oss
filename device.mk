3dfc8b07a5e5a606fb2f1244dbeae366c12b14b3
Marlon Alkan <info@maluu.eu>
2021-12-24 04:13:37 +0100

    monet: overlay: Merge "overlay-lineage" into "overlay"

--- device.mk
+++ device.mk
@@ -10,8 +10,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
