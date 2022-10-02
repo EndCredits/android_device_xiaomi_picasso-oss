874dc3962ca734620239d800de435f164eac0e5a
EndCredits <alicization.han@gmail.com>
2022-10-02 13:40:26 +0800

    picasso: Import AndroidProducts
    Signed-off-by: EndCredits <alicization.han@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_picasso.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_picasso-userdebug \
+    derp_picasso-eng
