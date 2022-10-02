eed262217d6b4d782791644d806cee1d86d34e51
EndCredits <alicization.han@gmail.com>
2022-10-02 18:39:17 +0800

    picasso: Declare xiaomi HALs in fcm
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,8 @@ TARGET_HAS_SIDEFPS := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	$(DEVICE_PATH)/xiaomi_framework_compatibility_matrix.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/picasso_user_defconfig
