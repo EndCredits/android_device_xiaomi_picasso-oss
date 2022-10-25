460e6ea378ebf7fb1be3b86c847937107114f35f
EndCredits <alicization.han@gmail.com>
2022-10-25 23:58:06 +0800

    picasso: Use correct kernel defconfig
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG := picasso_defconfig
+TARGET_KERNEL_CONFIG := vendor/picasso_user_defconfig
 
 # Inherit from the proprietary version
 include vendor/xiaomi/picasso/BoardConfigVendor.mk
