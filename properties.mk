7bb4a2c7c83bbd9adc9788d685c60c58fa704bf3
EndCredits <alicization.han@gmail.com>
2023-01-01 10:01:06 +0800

    picasso: Move lcd density prop to system properties
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- properties.mk
+++ properties.mk
@@ -3,8 +3,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.surface_flinger.has_HDR_display=true \
 	ro.surface_flinger.has_wide_color_display=true \
 	ro.surface_flinger.use_color_management=true \
-	ro.surface_flinger.wcg_composition_dataspace=143261696 \
-	qemu.sf.lcd_density=400 
+	ro.surface_flinger.wcg_composition_dataspace=143261696
+
+PRODUCT_SYSTEM_PROPERTIES += \
+	qemu.sf.lcd_density=440 \
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
